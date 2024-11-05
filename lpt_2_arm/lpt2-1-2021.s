6381e8ad1ea0baca00d677db1d1d3ee9200a1ca1
randyren278 <randyren278@gmail.com>
2024-11-05 10:46:06 -0800

    fix

--- lpt_2_arm/lpt2-1-2021.s
+++ lpt_2_arm/lpt2-1-2021.s
@@ -1,26 +1,27 @@
 .global func
 func:
     // Initialize return value as i + j
-    ADD R0, R0, R1          // R0 = i + j
+    ADD R4, R0, R1          // R4 = i + j, store in temp to avoid affecting R0 prematurely
 
     // Check if i < j
     CMP R0, R1              // Compare i and j
     BGE check_i_equals_k    // If i >= j, skip to next check
-    MOV R4, #1
-    STR R4, [R3]            // A[0] = 1
+    MOV R5, #1
+    STR R5, [R3]            // A[0] = 1
 
 check_i_equals_k:
     CMP R0, R2              // Compare i and k
     BNE end_func            // If i != k, return result
-    MOV R4, #2
-    STR R4, [R3, #4]        // A[1] = 2
+    MOV R5, #2
+    STR R5, [R3, #4]        // A[1] = 2
 
     // Check if A[2] > j
-    LDR R4, [R3, #8]        // Load A[2] into R4
-    CMP R4, R1              // Compare A[2] and j
+    LDR R5, [R3, #8]        // Load A[2] into R5
+    CMP R5, R1              // Compare A[2] and j
     BLE end_func            // If A[2] <= j, return result
-    MOV R4, #4
-    STR R4, [R3, #12]       // A[3] = 4
+    MOV R5, #4
+    STR R5, [R3, #12]       // A[3] = 4
 
 end_func:
+    MOV R0, R4              // Move i + j result into R0 for return
     MOV PC, LR              // Return from function
